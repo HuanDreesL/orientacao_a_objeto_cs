2f94178e80a2f1d629fd7ab58440fb551b1819b1
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-08-01 20:59:41 -0300

    banco_exercicio_2_semestre_2

---
+++ banco_exercicio_2_semestre_2/banco_exercicio_2_semestre_2/Banco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco_exercicio_2_semestre_2
+{
+    public class Banco
+    {
+        public int numeroConta { get; set; }
+        public string nome {  get; set; }
+        public double limite { get; set; }
+        public double saldo { get; set; }
+
+        public void Sacar(double valor) 
+        {
+            Console.WriteLine($"Sacando Valor de R$ {valor} da sua Conta.\n");
+            saldo -= valor;
+        }
+        public void Depositar(double valor) 
+        {
+            Console.WriteLine($"Depositando Valor de R$ {valor} da sua Conta.\n");
+            saldo += valor;
+        }
+
+        public void Consultar() 
+        {
+            Console.WriteLine("Dados da Conta Bancária");
+            Console.WriteLine($"Número Conta: {numeroConta}");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Limite da Conta: R$ {limite}");
+            Console.WriteLine($"Saldo Atual: R$ {saldo}");
+        }
+    }
+}
---
+++ banco_exercicio_2_semestre_2/banco_exercicio_2_semestre_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace banco_exercicio_2_semestre_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco huandres = new Banco();
+            
+            huandres.saldo = 500;
+            huandres.nome = "Huandres";
+            huandres.numeroConta = 146;
+            huandres.limite = 5000;
+
+            huandres.Sacar(250);
+            huandres.Depositar(250);
+            huandres.Consultar();
+        }
+    }
+}
