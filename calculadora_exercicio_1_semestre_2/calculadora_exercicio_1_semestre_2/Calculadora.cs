fbe6021c44ec3c109abaf23246ea90befb27151d
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-07-31 21:32:09 -0300

    calculadora_exercicio_1

---
+++ calculadora_exercicio_1_semestre_2/calculadora_exercicio_1_semestre_2/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace calculadora_exercicio_1_semestre_2
+{
+    public class Calculadora
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        public Calculadora(double num1, double num2) 
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public double Somar() 
+        {
+            return Num1 + Num2;
+        }
+    }
+}
+
---
+++ calculadora_exercicio_1_semestre_2/calculadora_exercicio_1_semestre_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+
+namespace calculadora_exercicio_1_semestre_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora(10, 35);
+            double resultado = calc.Somar();
+            Console.WriteLine(resultado);
+        }
+    }
+}
